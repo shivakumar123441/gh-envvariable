29192ae71f0ac73339f048aceb0d8d51b5e8cc7f
shivakumar123441 <75583406+shivakumar123441@users.noreply.github.com>
2024-06-22 17:49:51 +0530

    Update database.js [ skip ci ]

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `jdbc:mongodb://atlas-sql-66768d54b724cb4477964cd4-uv9dq.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
